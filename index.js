4bce6131fb1f6f49733a3297e37117c20fc66e9b
Franciszek Madej <franek@alergeek.ventures>
2022-04-27 12:02:23 +0200

    feat(eslint): review and add recommend / best practices

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ module.exports = {
     ////// ESLint recommended + best practices
     //
     ////// https://eslint.org/docs/2.0.0/rules/
-    //
 
+    // rules below: somewhat equal to eslint:recommended
     // warn on unused vars (from @typescript-eslint)
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "warn",
@@ -60,14 +60,92 @@ module.exports = {
     "no-ex-assign": "error",
     // disallow redundant negations for booleans (!!!foo)
     "no-extra-boolean-cast": "error",
-    // disallow redundant parentheses in code
-    "no-extra-parens": "error",
-    // disallow redundant semicolons
-    "no-extra-semi": "error",
     // disallow reassigning function declaration
     "no-func-assign": "error",
     // disallow declaring functions in any scope
     "no-inner-declarations": "error",
+    // lint regexes
+    "no-invalid-regexp": "error",
+    "no-regex-spaces": "error",
+    //  prevents problems with [a in b]
+    "no-negated-in-lhs": "error",
+    // warn about unreachable code
+    "no-unreachable": "warn",
+    // rules below: from ESLint's best practices
+    // if setter is defined - require getter too
+    "accessor-pairs": "error",
+    // require returns in maps, reduces etc
+    "array-callback-return": "error",
+    // no more than 10 logic branches
+    complexity: ["error", 10],
+    // either always return or never
+    "consistent-returns": "error",
+    // always use brackets for if statements
+    curly: "error",
+    // require default case in switches
+    "default-case": "error",
+    // prefer dot notation for objects (a['foo'] => a.foo)
+    "dot-notation": "error",
+    // enforce strong equality checks (!= => !==)
+    eqeqeq: "error",
+    // only allow variables declaration in case blocks
+    "no-case-declarations": "error",
+    // disallow weird syntax for regexes that can be mistaken for mathematical division
+    "no-div-regex": "error",
+    // disallow else after a return in an if
+    "no-else-return": "error",
+    // disallow use of empty destructuring patterns
+    "no-empty-pattern": "error",
+    // disallow use of eval() and similiar functions
+    "no-eval": "error",
+    "no-implied-eval": "error",
+    // disallow extending of built-ins
+    "no-extend-native": "error",
+    // prevent unnecessary function binding
+    "no-extra-bind": "error",
+    // prevent fallthrough from one switch case to other
+    "no-fallthrough": "error",
+    // prevent .5 or 2.
+    "no-floating-decimal": "error",
+    // prevent globals
+    "no-implicit-globals": "error",
+    // use readable type conversion methods
+    "no-implicit-coercion": "error",
+    // prevent misuse of this (outside of proper context)
+    "no-invalid-this": "error",
+    // no __iterator__
+    "no-iterator": "error",
+    // no labels
+    "no-labels": "error",
+    // prevent unnecessary blocks
+    "no-lone-blocks": "error",
+    // prevent magic numbers
+    "no-magic-numbers": "error",
+    // prevent misuse of wrappers
+    "no-new-wrappers": "error",
+    // prevent use of octal literals
+    "no-octal": "error",
+    // prevent reassignment of function parameters
+    "no-param-reassign": "error",
+    // prevent use of __proto__
+    "no-proto": "error",
+    // prevent assignments where values are equal
+    "no-self-assign": "error",
+    // prevent comparsion where values are equal
+    "no-self-compare": "error",
+    // prevent throwing non-error
+    "no-throw-literal": "error",
+    // prevent endless loops
+    "no-unmodified-loop-condition": "error",
+    // prevent unused statements
+    "no-unused-expressions": "error",
+    // ensure consistent logical expressions
+    yoda: "error",
+    // prevent variables shadowing
+    "no-shadow": "error",
+    // don't use var, use let only when modifying variable
+    "no-var": "error",
+    "prefer-const": "error",
 
     ////// Imports
     //
